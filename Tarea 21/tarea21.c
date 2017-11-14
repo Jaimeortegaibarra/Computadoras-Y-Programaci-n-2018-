a404778955dd9bd7c5b26d6c9237704ccc1fac3b
Jaime Jesus Ortega Ibarra <31601828+Jaimeortegaibarra@users.noreply.github.com>
2017-11-14 13:55:58 -0600

    Create tarea21.c

---
+++ Tarea 21/tarea21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "cabecera.h"
+int main(){
+	fnJ();
+	printf("\n");
+	fnA();
+	printf("\n");
+	fnI();
+	printf("\n");
+	fnM();
+	printf("\n");
+	fnE();
+	printf("\n");
+	return 0;
+}
+
