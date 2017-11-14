92327759ce13d10bdb46e9c6ad54aa2582dbe268
Jaime Jesus Ortega Ibarra <31601828+Jaimeortegaibarra@users.noreply.github.com>
2017-11-14 13:54:53 -0600

    Tarea 21

---
+++ Tarea 21/cabecera.h
@@ -0,0 +1,20 @@
+#ifndef CABECERA_H
+#define CABECERA_H
+void fnJ(){
+	printf("    J\n    J\n    J\nJ   J\nJJJJ\n");
+}
+void fnA(){
+	printf("AAAAA\nA   A\nAAAAA\nA   A\nA   A\n");
+}
+void fnI(){
+	printf("  I  \n  I  \n  I  \n  I  \n  I  \n"); 
+}
+void fnM(){
+	printf("MMMMM\nM M M\nM M M\nM   M\nM   M\n");
+}
+void fnE(){
+	printf("EEEEE\nE    \nEEEE \nE    \nEEEEE\n");
+}
+
+
+#endif
