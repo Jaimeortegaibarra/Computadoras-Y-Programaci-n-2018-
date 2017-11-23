9394573315222fa675fb14bb0e3442a6a9af8fa0
Jaime Jesus Ortega Ibarra <31601828+Jaimeortegaibarra@users.noreply.github.com>
2017-11-22 23:54:18 -0600

    Add files via upload

---
+++ Tarea 24/cabecera.h
@@ -0,0 +1,13 @@
+#ifndef CABECERA_H
+#define CABECERA_H
+void appInfoData(char *titulo,char *fecha)
+{
+	printf("************************************************\n");
+	printf("_\n");
+	printf("_       %s\n", titulo);
+	printf("_       Jaime Ortega Ibarra ®\n");
+	printf("_       %s\n", fecha);
+	printf("_\n");
+	printf("************************************************\n");
+}
+#endif
